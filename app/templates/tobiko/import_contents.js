a10bd5368be3cf9ba76605439323aff00a61b05c
Tri Nguyen <tri@tridnguyen.com>
2014-10-12 23:28:34 -0400

    add gfm option for marked

--- app/templates/tobiko/import_contents.js
+++ app/templates/tobiko/import_contents.js
@@ -1,17 +1,18 @@
 module.exports = {
-	options : {
-		baseDir: '<%= contentDir %>',
-		config : 'config.json',
-		markdown: {
-			breaks: true,
-			smartLists: true,
-			smartypants: true,
-			langPrefix: 'language-'
-		},
-		paginate: '<%= paginate %>'
-	},
-	all: {
-		src: '<%= contentDir %>/**/*.{json,md}',
-		dest: '<%= dataPath %>'
-	}
+  options : {
+    baseDir: '<%= contentDir %>',
+    config : 'config.json',
+    markdown: {
+      gfm: true,
+      breaks: true,
+      smartLists: true,
+      smartypants: true,
+      langPrefix: 'language-'
+    },
+    paginate: '<%= paginate %>'
+  },
+  all: {
+    src: '<%= contentDir %>/**/*.{json,md}',
+    dest: '<%= dataPath %>'
+  }
 }
